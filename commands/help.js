8d571d232be1fd5e3441e8b29bda7d5c11146e53
Joshua Ren <501stjoshua@gmail.com>
2023-06-14 05:54:00 +0800

    leaderboard edits

--- commands/help.js
+++ commands/help.js
@@ -17,9 +17,9 @@ module.exports = {
 		{ name: '</profile:1085225509870379100>', value: "View a debater's Ranked PDT profile (name, elo, record, etc.", inline: false},
 		{ name: '</leaderboard:1085371749018906735>', value: "View the leaderboard (based on elo)", inline: false},
 		{ name: '</qualified-leaderboard:1093725727154786426>', value: "View the leaderboard with only qualified debaters (3 or more wins)", inline: false},
-		{ name: '</round:1085371641044942879>', value: "View the details of a past round", inline: false},
+		{ name: '</round:1085371641044942879>', value: "View the details of a round", inline: false},
 		{ name: '</what-if:1087121663528804472>', value: "Tells debaters the change in elo that would occur based off of the results of a hypothetical round", inline: false},
-		{ name: '</update:1085225510327566457>', value: "Report the results of a round so that the bot can update debater's elos and record. You can only use /update for rounds that you debated in. After you use the command, the bot will ask the other debater to confirm the information before updating your profiles.", inline: false},
+		{ name: '</update:1085225510327566457>', value: "Report the results of a round so that the bot can update debater's elos and record. You can only use /update for rounds that you debated in. After you use the command, the bot will ask the other debater(s) to confirm the results before updating your profiles.", inline: false},
 		{ name: '</judged:1087559386223874109>', value: "Gain a 1.2x elo boost for judging a round. This boost will be used up in your next round. If you win that round you'll gain 1.2x more elo, and if you lose you'll lose the same amount of elo.", inline: false},
 
 	)
